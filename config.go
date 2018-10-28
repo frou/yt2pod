d5cd6432da66a8d5503ae1433ce94e8598a5ac48
Duncan Holm <duncan@holm.scot>
2018-10-28 11:53:04 +0000

    Validate everything inc whole-struct lvl constaint

--- config.go
+++ config.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"reflect"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/frou/poor-mans-generics/set"
@@ -14,37 +16,37 @@ import (
 
 type config struct {
 	// High-level
-	YTDataAPIKey           string    `json:"yt_data_api_key"`
-	Podcasts               []podcast `json:"podcasts"`
-	ServeHost              string    `json:"serve_host"`
-	ServePort              int       `json:"serve_port"`
-	ServeDirectoryListings bool      `json:"serve_directory_listings"`
+	YTDataAPIKey           string    `json:"yt_data_api_key"          validate:"required"`
+	Podcasts               []podcast `json:"podcasts"                 validate:"required,dive"`
+	ServeHost              string    `json:"serve_host"               validate:"hostname"`
+	ServePort              int       `json:"serve_port"               validate:"min=1,max=65535"`
+	ServeDirectoryListings bool      `json:"serve_directory_listings" validate:"-"`
 
 	// Watcher-related
-	CheckIntervalMinutes int    `json:"check_interval_minutes"`
-	YTDLFmtSelector      string `json:"ytdl_fmt_selector"`
-	YTDLWriteExt         string `json:"ytdl_write_ext"`
+	CheckIntervalMinutes int    `json:"check_interval_minutes" validate:"min=1"`
+	YTDLFmtSelector      string `json:"ytdl_fmt_selector"      validate:"required"`
+	YTDLWriteExt         string `json:"ytdl_write_ext"         validate:"alphanum"`
 }
 
 // ------------------------------------------------------------
 
 type podcast struct {
-	YTChannel             string `json:"yt_channel"`
+	YTChannel             string `json:"yt_channel" validate:"required"`
 	YTChannelID           string
 	YTChannelReadableName string
 
-	Name        string `json:"name"`
-	ShortName   string `json:"short_name"`
-	Description string `json:"description"`
+	Name        string `json:"name"        validate:"required"`
+	ShortName   string `json:"short_name"  validate:"required"`
+	Description string `json:"description" validate:"-"`
 
-	TitleFilterStr string `json:"title_filter"`
+	TitleFilterStr string `json:"title_filter" validate:"-"`
 	TitleFilter    *regexp.Regexp
 
 	EpochStr string `json:"epoch"`
 	Epoch    time.Time
 
-	Vidya           bool   `json:"vidya"`
-	CustomImagePath string `json:"custom_image"`
+	Vidya           bool   `json:"vidya" validate:"-"`
+	CustomImagePath string `json:"custom_image" validate:"-"`
 }
 
 func (p *podcast) feedPath() string {
@@ -71,11 +73,40 @@ func loadConfig(path string) (c *config, err error) {
 	if err := json.Unmarshal(buf, c); err != nil {
 		return nil, err
 	}
-	if err := validator.New().Struct(c); err != nil {
+
+	validate := validator.New()
+	epochDateRE := regexp.MustCompile(`^([[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2})?$`)
+	validate.RegisterValidation("epochdate", func(fl validator.FieldLevel) bool {
+		return epochDateRE.MatchString(fl.Field().String())
+	})
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+	validate.RegisterStructValidation(func(sl validator.StructLevel) {
+		c := sl.Current().Interface().(config)
+		var podcastShortNameSet set.Strings
+		for i := range c.Podcasts {
+			sn := c.Podcasts[i].ShortName
+			if podcastShortNameSet.Contains(sn) {
+				sl.ReportError(
+					c.Podcasts,
+					fmt.Sprintf("Podcasts[%d].ShortName", i),
+					"",
+					fmt.Sprintf("Multiple podcasts are using the same short name %q", sn),
+					"")
+				continue
+			}
+			podcastShortNameSet.Add(sn)
+		}
+	}, config{})
+	if err := validate.Struct(c); err != nil {
 		return nil, err
 	}
 
-	var podcastShortNameSet set.Strings
 	for i := range c.Podcasts {
 		// Parse Epoch
 		var t time.Time
@@ -96,14 +127,6 @@ func loadConfig(path string) (c *config, err error) {
 			return nil, err
 		}
 		c.Podcasts[i].TitleFilter = re
-
-		// Check for podcast shortname (in effect primary key) collisions.
-		sn := c.Podcasts[i].ShortName
-		if podcastShortNameSet.Contains(sn) {
-			return nil, fmt.Errorf(
-				"multiple podcasts using shortname \"%s\"", sn)
-		}
-		podcastShortNameSet.Add(sn)
 	}
 
 	return c, err
