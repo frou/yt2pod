459c5c66529fa9ba09f04fa61aa0b8d8394bdb01
Duncan Holm <duncan@holm.scot>
2022-10-06 18:38:44 +0100

    Comment

--- internal/xplatform/syslog_darwin.go
+++ internal/xplatform/syslog_darwin.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewSyslog(senderName string) (io.Writer, error) {
-	// macOS by default swallows LOG_INFO (and LOG_DEBUG) messages, so use the
-	// next highest severity.
+	// macOS by default drops messages with LOG_INFO and LOG_DEBUG severity (see the
+	// configuration in /etc/asl.conf), so use the least severe that isn't dropped.
 	return syslog.New(syslog.LOG_DAEMON|syslog.LOG_NOTICE, senderName)
 }
